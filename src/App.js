60fab330e367a1514a6afac739caa6d19eb9bb78
jooprima <jooprima@gmail.com>
2021-10-17 16:53:21 +0700

    folder structuring + import resolver

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import Homepage from "scenes/Homepage";
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Homepage></Homepage>
     </div>
   );
 }
